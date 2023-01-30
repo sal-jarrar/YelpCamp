3705136a51304f5c51e52ee48f7a4eb3182351a4
sal-jarrar <eng.sultanj@gmail.com>
2023-01-30 01:42:46 -0800

    server setup

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+import mysql from "mysql2";
+import env from "dotenv";
+env.config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+});
+
+export default pool.promise();
