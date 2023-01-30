863f7d0d89450322f5d8aee7af4ead47145cad5c
sal-jarrar <eng.sultanj@gmail.com>
2023-01-30 13:59:16 -0800

    add bootstrap,Header and Footer components.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
-import "./App.css";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Capmground</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Capmground</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,25 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Capmground</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+            <Navbar.Text>
+              Signed in as: <a href="#login">Mark Otto</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
