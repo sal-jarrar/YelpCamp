e02eef05d561ab01860d52f1d8eb1a9f1baa939d
sal-jarrar <eng.sultanj@gmail.com>
2023-01-31 01:42:54 -0800

    start with graphql

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
-import mysql from "mysql2";
-import env from "dotenv";
-env.config();
+import * as dotenv from "dotenv";
+dotenv.config();
+import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -9,4 +9,4 @@ const pool = mysql.createPool({
   password: process.env.DB_PASS,
 });
 
-export default pool.promise();
+export default pool;
--- server/server.js
+++ server/server.js
@@ -1,3 +1,33 @@
-import { ApolloServer } from "apollo-server";
+import * as dotenv from "dotenv";
+dotenv.config();
+import { ApolloServer, gql } from "apollo-server";
+import pool from "./config/db.js";
+
+const typeDefs = gql`
+  type Campground {
+    camp_id: ID!
+    title: String!
+    description: String!
+    image: String!
+    location: String!
+    price: Float!
+    created_at: String!
+    user_id: ID!
+  }
+  type Query {
+    getCapmgrounds: [Campground]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getCapmgrounds: async () => {
+      const [rows, fields] = await pool.query("SELECT * FROM campground");
+      return rows;
+    },
+  },
+};
 
 const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => console.log(`SERVER RUNNIG AT: ${url}`));
